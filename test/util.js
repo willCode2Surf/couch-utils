a1a2cb7ad6c1ba94c774c676da0d721835f38cf6
Dustin Brown <dustin.w.brown@gmail.com>
2014-05-08 15:54:49 -0500

    test testing

--- test/util.js
+++ test/util.js
@@ -1,32 +1,44 @@
-var util = require('../lib/util')
-var test = require('tap').test
+var util = require('../lib/util');
+var test = require('tap').test;
 
-test('oneCallback', function (t) {
-  t.plan(1)
 
-  var cb = function (er, data) {
-    t.ok(data)
-  }
-  var one = util.oneCallback(cb)
-  one(null, 'test')
-  one(new Error('NEVER CALL !! ERROR'))
+
+test('trivial', function(t){
+  t.plan(1);
+  t.notOk(false, "false should not be ok")
+  t.end()
 })
 
+  
+
+/*
+test('oneCallback', function (t) {
+  t.plan(1);
+
+  var cb = function (er, data) {
+    t.ok(data);
+  }
+  var one = util.oneCallback(cb)
+  one(null, 'test');
+  one(new Error('NEVER CALL !! ERROR'));
+});
+
 test('allCallback', function (t) {
-  t.plan(1)
+  t.plan(1);
 
   var cb = function (er, len) {
-    t.equal(len, 4)
+    t.equal(len, 4);
   }
-  var all = util.allCallback(cb)
+  var all = util.allCallback(cb);
 
   function doWork (cb) {
     setTimeout(function () {
       cb()
-    }, 100)
+    }, 100);
   }
-  doWork(all())
-  doWork(all())
-  doWork(all())
-  doWork(all())
-})
+  doWork(all());
+  doWork(all());
+  doWork(all());
+  doWork(all());
+});
+*/
